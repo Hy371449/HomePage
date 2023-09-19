e314430727470b65c8e921041820eb0300c6d1e4
huangyue <jon371449@gmail.com>
2023-09-19 11:33:25 -0700

    test

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
-const text = document.querySelector(".sec-text")
+const text = document.querySelector('.sec-text');
 const textLoad = () => {
   setTimeout(() => {
-    text.textContent = "Software Developer"
-  }, 1600)
+    text.textContent = 'Software Developer';
+  }, 1600);
 
   setTimeout(() => {
-    text.textContent = "Data Analyst"
-  }, 5600)
+    text.textContent = 'Data Analyst';
+  }, 5600);
 
   setTimeout(() => {
-    text.textContent = "Hoophead"
-  }, 9600)
+    text.textContent = 'Hoophead';
+  }, 9600);
 
   setTimeout(() => {
-    text.textContent = "Surfing Beginner"
-  }, 13600)
-}
+    text.textContent = 'Surfing Beginner';
+  }, 13600);
+};
 
-textLoad()
-setInterval(textLoad, 16000)
+textLoad();
+setInterval(textLoad, 16000);
 
-const contact = document.querySelector(".contact")
-const socialMedia = document.querySelector(".social-media")
-contact.addEventListener("click", () => {
-  if (socialMedia.classList.contains("display")) {
-    socialMedia.classList.remove("display")
-    contact.textContent = "Contact Me"
+const contact = document.querySelector('.contact');
+const socialMedia = document.querySelector('.social-media');
+contact.addEventListener('click', () => {
+  if (socialMedia.classList.contains('display')) {
+    socialMedia.classList.remove('display');
+    contact.textContent = 'Contact Me';
   } else {
-    socialMedia.classList.add("display")
-    contact.textContent = "Back"
+    socialMedia.classList.add('display');
+    contact.textContent = 'Back';
   }
-})
+});
